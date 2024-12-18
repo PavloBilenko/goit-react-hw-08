d2a75925e02f818d2348c809ef9375e3c1f123e7
PavloBilenko <bilenko.pavlo@gmail.com>
2024-12-18 21:24:31 +0100

    -

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,16 +1,24 @@
 // src/components/LogoutButton/LogoutButton.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../redux/auth/operation';
+import { logOut } from '../../redux/auth/operations'; // Імпортуємо logOut
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { useNavigate } from 'react-router-dom';
 import s from './LogoutButton.module.css';
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(logout());
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/'); // Перенаправляємо на домашню сторінку, коли користувач більше не залогінений
+    }
+  }, [isLoggedIn, navigate]);
+
+  const handleLogout = async () => {
+    await dispatch(logOut()); // Викликаємо логаут
   };
 
   if (!isLoggedIn) {
