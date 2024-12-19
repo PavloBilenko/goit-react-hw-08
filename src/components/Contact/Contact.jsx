737c439163ac9ca6abf12bf0b0f5ff5f22c8b4c4
PavloBilenko <bilenko.pavlo@gmail.com>
2024-12-19 11:12:33 +0100

    -

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,28 +2,43 @@ import { FaUser, FaPhone } from 'react-icons/fa';
 import s from './Contact.module.css';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/operations';
+import toast from 'react-hot-toast'; // Імпорт функції toast
 
 function Contact({ contact }) {
   const dispatch = useDispatch();
+
   const handleClick = () => {
-    dispatch(deleteContact(contact.id));
+    dispatch(deleteContact(contact.id))
+      .then(() => {
+        // Відображення успішного повідомлення
+        toast.success(`Contact "${contact.name}" was deleted successfully!`);
+      })
+      .catch((error) => {
+        // Відображення повідомлення про помилку
+        toast.error(`Failed to delete contact: ${error.message}`);
+      });
   };
+
   return (
     <>
-      <div className={s.item}>
-        <p className={s.text_name}>
-          <FaUser className={s.icon} />
-          {contact.name}:
-        </p>
-        <p className={s.text_number}>
-          <FaPhone className={s.icon} />
-          {contact.number}
-        </p>
+      <div className={s.wrapper}>
+        <div className={s.contact}>
+          <p className={s.text_name}>
+            <FaUser className={s.icon} />
+            {contact.name}:
+          </p>
+          <p className={s.text_number}>
+            <FaPhone className={s.icon} />
+            {contact.number}
+          </p>
+        </div>
+
+        <button type="button" className={s.button} onClick={handleClick}>
+          Delete
+        </button>
       </div>
-      <button type="button" className={s.btn} onClick={handleClick}>
-        Delete
-      </button>
     </>
   );
 }
+
 export default Contact;
